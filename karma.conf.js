1f84f142735a34d56f1ed0c53c660b8986a88d1d
Arathi Sreekumar <arathi@Arathis-MacBook-Air.local>
2016-07-19 02:35:33 +0100

    Adding Karma testing framework

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', 'requirejs'],
+    files: [
+      'test/test-main.js',
+      'js/compiled/compiled.min.js',
+      { pattern: 'js/application/**/*.js', included: false },
+      { pattern: 'templates/**/*.html', included: false },
+      { pattern: 'js/libs/**/*.js', included: false },
+      { pattern: 'test/**/*.spec.js', included: false }
+    ],
+    exclude: [ 'js/main.js' ],
+    preprocessors: {
+      'app/**/*.js': ['coverage']
+    },
+    reporters: ['progress', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    coverageReporter: {
+      type: 'html',
+      dir: 'test/coverage/'
+    }
+  });
+};
---
+++ test/test-main.js
@@ -0,0 +1,41 @@
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
+
+var pathToModule = function(path) {
+  var returnValue = path.replace(/^\/base\//, '').replace(/\.js$/, '');
+  return returnValue;
+};
+
+Object.keys(window.__karma__.files).forEach(function(file) {
+  if (TEST_REGEXP.test(file)) {
+    allTestFiles.push(pathToModule(file));
+  }
+});
+
+require.config({
+  baseUrl: '/base/application',
+  urlArgs: 'cb=' + Math.random(),
+  paths: {
+    text: '../js/libs/text',
+    jquery: '../js/libs/jquery-3.0.0.min',
+    underscore: '../js/libs/underscore.min',
+    backbone: '../js/libs/backbone.min',
+    handlebars: '../js/libs/handlebars.min',
+    router: '../js/application/router',
+    templates: '../templates',
+    views: '../js/application/views',
+    models: '../js/application/models',
+    collections: '../js/application/collections',
+    //helper: '../test/helpers',
+    test: '../test'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    }
+  },
+  deps: allTestFiles,
+  callback: window.__karma__.start
+});
+
