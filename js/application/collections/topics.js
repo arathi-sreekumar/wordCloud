939efb54ec69e3f3e3c79badd59c610ae7844ccc
Arathi Sreekumar <arathi@Arathis-MacBook-Air.local>
2016-07-19 23:44:06 +0100

    Added topics collection to the project

---
+++ js/application/collections/topics.js
@@ -0,0 +1,131 @@
+'use strict';
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'models/topic'
+], function($, _, Backbone, TopicModel){
+
+  //Private variables
+  var maxVolume, minVolume;
+  var fontSizeCategories = ['xs', 's', 'm', 'l', 'xl', 'xxl'];
+
+  //Private functions
+  /*
+   * Function that gets n log intervals between a start value and an end value
+   * @param total_intervals   the total number of intervals we require
+   * @param start   the starting point for our interval (lowest value of our data)
+   * @param end   the last point for our interval (highest value in our data)
+  */
+  function getLogIntervals(total_intervals, start, end) {
+    start = start || 0.1;
+    var startInterVal = 1, endInterval = total_intervals,
+        minLog = Math.log(start), maxLog = Math.log(end),
+        scale = (maxLog-minLog) / (endInterval-startInterVal),
+        result = [], logPoint, i;
+
+    for (i = 1; i < total_intervals; i++) {
+      logPoint = Math.exp(minLog + scale*(i - startInterVal));
+      result.push(Math.round(logPoint));
+    }
+
+    result.push(end);
+    return result;
+  }
+
+  var ProjectsCollection = Backbone.Collection.extend({
+    model: TopicModel,
+
+    url: '/data/topics.json',
+
+    parse: function (data) {
+      var topics = this.orderTopicsByFrequency(data.topics);
+      topics = this.addCloudWeightAndSentimentToTopics(topics);
+
+      return topics;
+    },
+    
+    initialize: function(noOfGroups){
+      this.noOfGroups = noOfGroups || 6;
+    },
+
+    /* 
+     * Ordering topics by decreasing order of volume
+     * @param: topics   list of all topics for the cloud
+     * @return: topcis   sorted by volume
+    */
+    orderTopicsByFrequency: function (topics) {
+      topics = _.sortBy(topics, function (topic) {
+        return -topic.volume;
+      });
+    
+      return topics;
+    },
+
+    /*
+     * Add cloud weight to topics
+     * @param: topics   list of all topics for the cloud sorted in descending order of frequency
+    */
+    addCloudWeightAndSentimentToTopics: function (topics) {
+      var that = this;
+      this.maxVolume = topics[0].volume;
+      this.minVolume = topics[topics.length - 1].volume;
+
+      _.each(topics, function (topic) {
+          topic = that.addLogarithmicCloudWeightToTopics(topic);
+          topic = that.addDominatingSentimentToTopic(topic);
+      });
+      return topics;
+    },
+
+    /*
+     * Add dominating sentiment for each topic
+     * @param topic  the topic for hich the dominating sentiment score will be calculated
+    */
+    addDominatingSentimentToTopic: function (topic) {
+      //Adding a dominating setinment attribute to topic
+      if (topic.sentimentScore > 60) {
+        topic.dominatingSentiment = 'positive';
+      } else if (topic.sentimentScore < 40) {
+        topic.dominatingSentiment = 'negative';
+      } else {
+        topic.dominatingSentiment = 'neutral';
+      }
+      return topic;
+    },
+
+    /*
+     * add linear cloud weight info for fontsizes to a topic
+     * @param: topic   list of all topics for the cloud
+     * @return topic   updated topic with cloud weight information
+    */
+    addLinearCloudWeightToTopic: function (topic) {
+      var step = (this.maxVolume - this.minVolume) / (this.noOfGroups - 1),
+          weightGroup = Math.floor((topic.volume - this.minVolume) / step);
+
+      topic.cloudWeightGroup = fontSizeCategories[weightGroup];
+
+      return topic;
+    },
+
+    /*
+     * Add logarithmic weights for fontsizes
+     * @param: topics   list of all topics for the cloud
+     * @return topic   updated topic with cloud weight information
+    */
+    addLogarithmicCloudWeightToTopics: function (topic) {
+      var logIntervals = getLogIntervals(this.noOfGroups, this.minVolume, this.maxVolume);
+      var interval;
+      for (interval = 0; interval < logIntervals.length; interval++) {
+        if (topic.volume <= logIntervals[interval]) {
+          topic.cloudWeightGroup = fontSizeCategories[interval];
+          return topic;
+        }
+      }
+    }
+
+
+  });
+ 
+  return ProjectsCollection;
+});
---
+++ test/application/collections/topics.spec.js
@@ -0,0 +1,119 @@
+
+'use strict';
+define([
+  'underscore',
+  'backbone',
+  'sinon',
+  'collections/topics'
+], function(_, Backbone, sinon, TopicsCollection) {
+
+  var mockTopicsData = {topics: [
+        {"label":"Berlin","volume":165,"sentimentScore":65,"sentiment":{"negative":3,"neutral":133,"positive":29}},
+        {"label":"Kantine am Berghain","volume":11,"sentimentScore":59,"sentiment":{"neutral":10,"positive":1}},
+        {"label":"London","volume":11,"sentimentScore":77,"sentiment":{"neutral":8,"positive":3}},
+        {"label":"UK","volume":8,"sentimentScore":50,"sentiment":{"neutral":8}},
+        {"label":"Marcel Dettmann","volume":8,"sentimentScore":87,"sentiment":{"neutral":5,"positive":3}},
+        {"label":"Disco","volume":8,"sentimentScore":50,"sentiment":{"neutral":8}},
+        {"label":"Barcelona","volume":7,"sentimentScore":50,"sentiment":{"neutral":7}},
+        {"label":"D/B Presents","volume":3,"sentimentScore":50,"sentiment":{"neutral":3}},
+        {"label":"Jun","volume":5,"sentimentScore":50,"sentiment":{"neutral":5}},
+        {"label":"DJ","volume":48,"sentimentScore":54,"sentiment":{"neutral":46,"positive":2}},
+        {"label":"Mixes","volume":5,"sentimentScore":50,"sentiment":{"neutral":5}},
+        {"label":"Panorama Bar Music","volume":5,"sentimentScore":50,"sentiment":{"neutral":5}},
+        {"label":"Terrace Sundae","volume":5,"sentimentScore":50,"sentiment":{"neutral":5}},
+        {"label":"Berghain resident","volume":13,"sentimentScore":73,"sentiment":{"neutral":10,"positive":3}},
+        {"label":"Germany","volume":13,"sentimentScore":80,"sentiment":{"neutral":9,"positive":4}},
+        {"label":"Amsterdam","volume":12,"sentimentScore":91,"sentiment":{"neutral":7,"positive":5}},
+        {"label":"Ostgut Ton","volume":24,"sentimentScore":58,"sentiment":{"neutral":22,"positive":2}},
+        {"label":"Code","volume":16,"sentimentScore":68,"sentiment":{"neutral":13,"positive":3}},
+        {"label":"Quantified Drunk","volume":14,"sentimentScore":50,"sentiment":{"neutral":14}},
+        {"label":"Watergate","volume":7,"sentimentScore":64,"sentiment":{"neutral":6,"positive":1}},
+        {"label":"debut LP","volume":6,"sentimentScore":100,"sentiment":{"neutral":3,"positive":3}},
+        {"label":"Patrick Gräser","volume":6,"sentimentScore":100,"sentiment":{"neutral":3,"positive":3}},
+        {"label":"Hammered","volume":48,"sentimentScore":20,"sentiment":{"neutral":18,"negative":30}},
+        {"label":"Panorama Bar in Berlin","volume":6,"sentimentScore":83,"sentiment":{"neutral":4,"positive":2}},
+        {"label":"San Soda's Panorama Bar","volume":13,"sentimentScore":50,"sentiment":{"neutral":13}},
+        {"label":"legendary nightclub","volume":6,"sentimentScore":150,"sentiment":{"positive":6}},
+        {"label":"Ben Klock","volume":5,"sentimentScore":50,"sentiment":{"neutral":5}},
+        {"label":"club culture","volume":3,"sentimentScore":50,"sentiment":{"neutral":3}},
+        {"label":"Live set","volume":4,"sentimentScore":75,"sentiment":{"neutral":3,"positive":1}},
+        {"label":"dance music","volume":4,"sentimentScore":50,"sentiment":{"neutral":4}}  
+    ]};
+
+  describe('Collections: TopicsCollection', function() {
+
+    beforeEach(function () {
+      this.topics = new TopicsCollection();
+    });
+
+    it('should be defined', function() {
+      expect(TopicsCollection).toBeDefined();
+    });
+
+    it('can be instantiated', function() {
+      var topics = new TopicsCollection();
+      expect(topics).not.toBeNull();
+    });
+
+    describe('#fetch', function() {
+      beforeEach(function () {
+        this.server = sinon.fakeServer.create();
+      });
+      afterEach(function() {
+        this.server.restore();
+      });
+
+      it ('server exists', function() {
+        this.topics.fetch();
+        expect(this.server.requests.length).toEqual(1);
+        expect(this.server.requests[0].method).toEqual("GET");
+        expect(this.server.requests[0].url).toEqual("/data/topics.json");
+      });
+
+      describe('request', function() {
+        beforeEach(function() {
+          this.server.respondWith('GET', '/data/topics.json', [
+            200,
+            { "Content-Type": "application/json" },
+            JSON.stringify(mockTopicsData)
+          ]);
+          this.topics.fetch();
+          this.server.respond();
+        });
+
+        it('check if topics were fetched', function() {
+          expect(this.topics.models).toBeDefined();
+          expect(this.topics.length).toEqual(mockTopicsData.topics.length);
+        });
+
+        it('check whether the result is ordered by volume in descending order', function () {
+            var randomPosition = Math.floor(Math.random() * mockTopicsData.topics.length);
+            expect(this.topics.at(randomPosition).get('volume')).not.toBeLessThan(this.topics.at(randomPosition + 1).get('volume'));
+        });
+
+        it('check whether the weight for the biggest volume word is highest', function () {
+          expect(this.topics.at(0).get('cloudWeightGroup')).toEqual('xxl'); //get this value from settings
+        });
+
+        it('check whether the weight for the smallest volume word is lowest', function () {
+          expect(this.topics.at(mockTopicsData.topics.length - 1).get('cloudWeightGroup')).toEqual('xs'); //get this value from settings
+        });
+
+        it('check whether the dominating sentiment is calculated correctly for a random sampling', function () {
+          var randomPosition = Math.floor(Math.random() * mockTopicsData.topics.length) + 1;
+          var sentimentScore = this.topics.at(randomPosition).get('sentimentScore');
+          var dominating_sentiment = 'neutral';
+          if (sentimentScore > 60) {
+            dominating_sentiment = 'positive';
+          } else if (sentimentScore <  40) {
+            dominating_sentiment = 'negative';
+          }
+          expect(this.topics.at(randomPosition).get('dominatingSentiment')).toEqual(dominating_sentiment);
+        });
+
+      });
+
+    });
+  });
+
+});
