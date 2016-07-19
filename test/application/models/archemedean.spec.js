1a3b5cc5c7fae090939fa27bd37884788353f2fa
Arathi Sreekumar <arathi@Arathis-MacBook-Air.local>
2016-07-19 13:52:39 +0100

    Updated test for archemedean spiral

--- test/application/models/archemedean.spec.js
+++ test/application/models/archemedean.spec.js
@@ -16,6 +16,14 @@ define([
       expect(archemedeanModel).not.toBeNull();
     });
 
+    it('gets a spiral position for first word', function() {
+      var rectBounds = { x: 10, y: 10, height: 100, width: 100 };
+      var archemedeanModel = new ArchemedeanModel(rectBounds);
+      var coordinates = archemedeanModel.getNextSpiralPosition();
+      expect(coordinates.x).toBeDefined();
+      expect(coordinates.y).toBeDefined();
+    });
+
     it('generates valid points for case ', function() {
     	// To be done
     });
