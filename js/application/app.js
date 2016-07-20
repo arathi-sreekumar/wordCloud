403f4e17c2763e680debf92fc35916777e9fcc8e
Arathi Sreekumar <arathi@Arathis-MacBook-Air.local>
2016-07-20 23:23:52 +0100

    Added spacing

--- js/application/app.js
+++ js/application/app.js
@@ -6,7 +6,7 @@ define([
   'underscore', 
   'backbone',
   'router', // Request router.js
-], function($, _, Backbone, Router){
+], function ( $, _, Backbone, Router ) {
 
   var initialize = function(){
     // Pass in our Router module and call it's initialize function
