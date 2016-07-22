c71e8e4816d28f4ec4accc4aad9966f3e6898318
Arathi Sreekumar <arathi@Arathis-MacBook-Air.local>
2016-07-22 02:46:43 +0100

    Added router test

---
+++ test/README.md
@@ -0,0 +1,3 @@
+###test folder
+The folder for the test driver's (Karma) bootstrap for RequireJS with Jasmine and sinon 
+and contained specs for this lab 
---
+++ test/application/router.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'sinon',
+  'router'
+], function($, _, Backbone, sinon, Router) {  
+
+	describe('Application routes', function() {
+	    beforeEach(function() {
+	        //Router.initialize();
+	        this.router = new Router.initialize();
+		    try {
+		      Backbone.history.start({silent:true, pushState:true});
+		    } catch(e) {}
+		    this.router.navigate('elsewhere');
+	    });
+
+	    it('should exist', function () {
+	    	expect(this.router).toBeDefined();
+	    })
+
+	    //to do test different routes
+
+	    afterEach(function() {
+	        Backbone.history.stop();
+	    });
+
+	});
+});
